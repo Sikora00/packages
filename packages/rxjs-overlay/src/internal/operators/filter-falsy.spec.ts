167a48be72ef0deb62cb08e08588d2909881ef12
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-04-13 21:36:04 +0200

    test(rxjs-overlay): fix tests

--- packages/rxjs-overlay/src/internal/operators/filter-falsy.spec.ts
+++ packages/rxjs-overlay/src/internal/operators/filter-falsy.spec.ts
@@ -2,7 +2,7 @@ import { cold } from 'jest-marbles';
 import { filterFalsy } from './filter-falsy';
 
 describe('test filter falsy', () => {
-  test('emits truthy value', () => {
+  it('emits truthy value', () => {
     const value = {};
     const expects = cold('-a', { a: value });
     expect(cold('-a', { a: value }).pipe(filterFalsy)).toBeObservable(expects);
