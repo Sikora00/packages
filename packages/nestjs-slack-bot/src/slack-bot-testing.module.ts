261b741476e71134a4054de2225b749b67247d1b
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-09-27 15:47:43 +0200

    fix(): remove forRoot from testing module

--- packages/nestjs-slack-bot/src/slack-bot-testing.module.ts
+++ packages/nestjs-slack-bot/src/slack-bot-testing.module.ts
@@ -1,34 +1,20 @@
-import { DynamicModule, Module, OnModuleInit } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { Bot } from './bot';
 import { HelpSlackCommand } from './commands/help.slack-command';
 import { InterceptorsConsumer } from './interceptors.consumer';
-import { SlackBotModuleOptions } from './interfaces/slack-bot-module-options.interface';
 import { SlackTestingService } from './slack-testing.service';
 import { SlackService } from './slack.service';
 
-const SLACK_TOKEN = Symbol('SLACK_TOKEN');
-
-@Module({})
-export class SlackBotTestingModule implements OnModuleInit {
-  static forRoot(options: SlackBotModuleOptions): DynamicModule {
-    return {
-      module: SlackBotTestingModule,
-      providers: [
-        Bot,
-        HelpSlackCommand,
-        InterceptorsConsumer,
-        {
-          provide: SlackService,
-          useClass: SlackTestingService,
-        },
-      ],
-      exports: [SlackService],
-    };
-  }
-
-  constructor(private bot: Bot) {}
-
-  onModuleInit(): void {
-    this.bot.init();
-  }
-}
+@Module({
+  providers: [
+    Bot,
+    HelpSlackCommand,
+    InterceptorsConsumer,
+    {
+      provide: SlackService,
+      useClass: SlackTestingService,
+    },
+  ],
+  exports: [SlackService],
+})
+export class SlackBotTestingModule {}
