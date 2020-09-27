6e026114a9ae54e39e32a1c279fcc2c9e9aa3587
Maciej Sikorski <maciejsikorski00@gmail.com>
2020-09-27 14:23:44 +0200

    test(test): add testing module
    Add testing module to avoid slack connection

---
+++ packages/nestjs-slack-bot/src/slack-bot-testing.module.ts
@@ -0,0 +1,33 @@
+import { DynamicModule, Module, OnModuleInit } from '@nestjs/common';
+import { Bot } from './bot';
+import { HelpSlackCommand } from './commands/help.slack-command';
+import { InterceptorsConsumer } from './interceptors.consumer';
+import { SlackBotModuleOptions } from './interfaces/slack-bot-module-options.interface';
+import { SlackTestingService } from './slack-testing.service';
+import { SlackService } from './slack.service';
+
+const SLACK_TOKEN = Symbol('SLACK_TOKEN');
+
+@Module({})
+export class SlackBotTestingModule implements OnModuleInit {
+  static forRoot(options: SlackBotModuleOptions): DynamicModule {
+    return {
+      module: SlackBotTestingModule,
+      providers: [
+        Bot,
+        HelpSlackCommand,
+        InterceptorsConsumer,
+        {
+          provide: SlackService,
+          useClass: SlackTestingService,
+        },
+      ],
+    };
+  }
+
+  constructor(private bot: Bot) {}
+
+  onModuleInit(): void {
+    this.bot.init();
+  }
+}
